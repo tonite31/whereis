ab86b876d831ac6bc9b068a6b2506329b62de5e8
Alprensia <Alprensia>
2016-04-13 18:11:50 +0900

    add bootstrap. change characterset

--- controller/router/MainRouter.js
+++ controller/router/MainRouter.js
@@ -2,6 +2,11 @@ module.exports = function(app)
 {
 	app.get('/', function(req, res, next)
 	{
-		res.render('index');
+		res.render('layout', {page : 'index'});
+	});
+	
+	app.get('/:id', function(req, res, next)
+	{
+		res.render('layout', {page : req.params.id});
 	});
 }
